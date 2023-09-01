2848f172971cb5dc2ac1d8ce72664a8ce29d7501
Apxy <ckc.zachary@yahoo.com>
2023-09-01 16:59:19 +0800

    added more param changes, working on rcoeffs table

--- sketch.js
+++ sketch.js
@@ -72,15 +72,15 @@ function isCharNumber(c) {
   return c >= '0' && c <= '9';
 }
 
-function styleinput(input, x,y, settings, label){
+function styleinput(input, x,y, main, label, w = "7"){
     let maininput = createDiv();
     //bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32
       maininput.class("mb-4 h-10 flex ");
-      maininput.parent(settings);
+      maininput.parent(main);
       maininput.position( x,y);
       let labelinput = createDiv(label);
       labelinput.parent(maininput);
-      labelinput.class(" w-7 h-10 text-gray-500 dark:text-gray-400 inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600");
+      labelinput.class(" w-" +w+ " h-10 text-gray-500 dark:text-gray-400 inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600");
       //let x = createInput(params[i+1][1].x);
       input.style("width", "4em");
       // x.style("margin-left", "1em");
@@ -99,13 +99,43 @@ function styleinput(input, x,y, settings, label){
     // input.position(x,y);
     // input.parent(settings);
 }
-function setting(params){ //convert everything in terms of displayheight and displaywidth
+function styleinput2(input, x,y, main, label, w = "7"){
+    let maininput = createDiv();
+    //bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32
+      maininput.class("mb-4 h-10 flex ");
+      //maininput.parent(main);
+      maininput.position( x,y);
+      let labelinput = createDiv(label);
+      labelinput.parent(maininput);
+      labelinput.class(" w-" +w+ " h-10 text-gray-500 dark:text-gray-400 inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600");
+      //let x = createInput(params[i+1][1].x);
+      input.style("width", "4em");
+      // x.style("margin-left", "1em");
+      // x.style("margin-right", "1em");
+      // x.style("margin-bottom", "0.5em");
+      // x.style("border-radius", "0.5em");
+      input.parent(maininput);
+      input.class("h-10 rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
+
+
+    // input.style("width", "4em");
+    // input.style("margin-left", "1em");
+    // input.style("margin-right", "1em");
+    // input.style("margin-bottom", "0.5em");
+    // input.class("block w-full p-4 h-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
+    // input.position(x,y);
+    // input.parent(settings);
+}
+
+function setting(params, parent){ //convert everything in terms of displayheight and displaywidth
   let settings = createDiv();
-  settings.position(windowWidth * 0.8, 0);
-  settings.style("height",  '100vh');
+  //settings.parent(parent);
+  //settings.position(0,0);
+   settings.position(windowWidth * 0.8, 0);
+  settings.style("height",  '80vh');
   settings.style("width", '20vw');
   settings.style("background: linear-gradient(to bottom, #183D54, #052F58);");
-  settings.style("border rounded-sm transform scale-0 absolute transition duration-150 ease-in-out origin-top min-w-32")
+  //settings.class("border rounded-sm transform scale-0 absolute transition duration-150 ease-in-out origin-top min-w-32")
   if (params.length > 0 && selected != -1){
 for (let i = 0; i < params.length - 1; i++){
     let label = createSpan(params[i+1][0]);
@@ -119,7 +149,7 @@ for (let i = 0; i < params.length - 1; i++){
     // label.style("padding-right", "0.5em");
     label.class("block mb-2 text-sm font-medium text-gray-900 dark:text-white");
     label.parent(settings);
-    label.position( 10, 320 + 80 * (i));
+    label.position( 10, 50 + 80 * (i));
     label.innerHTML = params[i+1][0];
     if (params[i+1][0] == "Position"){
       // let mainx = createDiv();
@@ -135,7 +165,7 @@ for (let i = 0; i < params.length - 1; i++){
       // x.style("margin-right", "1em");
       // x.style("margin-bottom", "0.5em");
       // x.style("border-radius", "0.5em");
-      styleinput(x, displayWidth / 200, 300 + 80 * (i) + 50, settings, "x:");
+      styleinput(x, displayWidth / 200, 30 + 80 * (i) + 50, settings, "x:");
       //x.class("h-10 rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
       //x.parent(mainx);
       // x.style("border", "none");
@@ -162,7 +192,7 @@ for (let i = 0; i < params.length - 1; i++){
       // // y.style("padding-top", "0.5em");
       // // y.style("padding-bottom", "0.5em");
       // y.position( displayWidth / 20 , 300 + 80 * (i) + 50);
-      styleinput(y, displayWidth / 15 , 300 + 80 * (i) + 50, settings, "y:");
+      styleinput(y, displayWidth / 15 , 30 + 80 * (i) + 50, settings, "y:");
       //y.parent(settings);
       x.input(function () {
         if (isCharNumber(findDifference( str(params[i+1][1].x), x.value())) || isCharNumber(findDifference( x.value(), str(params[i+1][1].x)))){
@@ -192,9 +222,9 @@ for (let i = 0; i < params.length - 1; i++){
       x.attribute("data-bodyindex", selected);
       y.attribute("data-bodyindex", selected);
     } else if (params[i+1][0] == "Angle"){
-      let angle = createSlider(0,360,params[i+1][1].theta,1);
+      let angle = createSlider(0,360,params[i+1][1],1);
       //styleinput(angle, windowWidth / 200, 300 + 70 * (i) + 50, settings);
-      angle.position(windowWidth / 100, 300 + 80 * (i) + 50);
+      angle.position(windowWidth / 100, 30 + 80 * (i) + 65);
       //angle.class("block mb-2 text-sm font-medium text-gray-900 dark:text-white");
       angle.parent(settings);
       angle.class("w-4/5 h-2 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 accent-blue-500");
@@ -206,6 +236,83 @@ for (let i = 0; i < params.length - 1; i++){
         
         updateComponents();
       });
+  } else if (params[i+1][0] == "Width"){
+    let width = createInput(params[i+1][1]);
+    width.style("width", "4em");
+    styleinput(width, windowWidth / 200, 30 + 80 * (i) + 50, settings, "Width:", "20");
+    width.input(function () {
+        if (isCharNumber(findDifference( str(params[i+1][1]), width.value())) || isCharNumber(findDifference( width.value(), str(params[i+1][1])))){
+        if (width.value() == "" || int(width.value()) == 0){
+          totalarr[selected].coords.magnitude = 5;
+          //totalarr[selected].setPos(int(x.value()), 0);
+        } else {
+          totalarr[selected].coords.magnitude = int(width.value()) / 2;
+          //console.log(totalarr[selected].coords.magnitude)
+        }
+        updateComponents();
+      } else {
+        width.value(params[i+1][1]);
+      }
+      });
+  } else if (params[i+1][0] == "Thickness"){
+    //console.log(params[i+1][1])
+    let thickness = createInput(params[i+1][1]);
+    thickness.style("width", "4em");
+    styleinput(thickness, windowWidth / 200, 30 + 80 * (i) + 50, settings, "Thickness:", "20");
+    thickness.input(function () {
+        if (isCharNumber(findDifference( str(params[i+1][1]), thickness.value())) || isCharNumber(findDifference( thickness.value(), str(params[i+1][1])))){
+        if (thickness.value() == "" || int(thickness.value()) == 0){
+          totalarr[selected].thickness = 5;
+          //totalarr[selected].setPos(int(x.value()), 0);
+        } else {
+          totalarr[selected].thickness = int(thickness.value()) / 2;
+          //console.log(totalarr[selected].coords.magnitude)
+        }
+        updateComponents();
+      } else {
+        thickness.value(params[i+1][1]);
+      }
+      });
+  } else if (params[i+1][0] == "Refractive Coefficients"){
+    let rcoeffs = createElement("table");
+    
+    rcoeffs.position(windowWidth / 200, 30 + 80 * (i) + 50);
+    rcoeffs.class("flex flex-col overflow-hidden bg-gray-300 h-1/3 w-full text-center text-sm font-light rounded-lg");
+
+    let thead = createElement("tr");
+    thead.parent(rcoeffs);
+    thead.class("font-medium");
+
+    let b = createElement("th");
+    b.class("px-6 py-4 bg-gray-100 text-gray-600 ");
+    b.attribute("scope", "col");
+    let bval = createSpan("B Coefficients");
+    //bval.class()
+    b.child(bval);
+    //b.value("B Coefficients");
+    // let b1input = createInput(params[i+1][1][0][0]); //b1 coefficient
+    // b1input.style("width", "4em");
+    
+    // styleinput2(b1input, windowWidth / 200, 0,0, b1, "B1:", "20");
+
+    // b1.child(b1input);
+    thead.child(b);
+
+
+    let c = createElement("th");
+    c.class("px-6 py-4 bg-gray-100 text-gray-600 border-b border-gray-200 font-bold uppercase text-sm text-left");
+    c.attribute("scope", "col");
+    let cval = createSpan("C Coefficients");
+    c.child(cval);
+    // let b2input = createInput(params[i+1][1][0][0]); //c1 coefficient
+    // b2input.style("width", "4em");
+    // b2.child(b2input);
+    // styleinput2(b2input, windowWidth / 200, 0,0, b2, "B2:", "20");
+    thead.child(c);
+
+
+    //rcoeffs.child(thead)
+    rcoeffs.parent(settings);
   }
   }
 }
@@ -216,8 +323,13 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 
+
   sandbox = createGraphics(windowWidth * 0.8, windowHeight * 0.85);
-  setting(editables);
+  setting(editables, document.getElementById("parameters-panel"));
+  // settings.position(windowWidth * 0.8, 0);
+  // settings.style("height",  '100vh');
+  // settings.style("width", '20vw');
+  // settings.style("background: linear-gradient(to bottom, #183D54, #052F58);");
   //frameRate(1)
   //ray1 = new RayComponent(createVector(50,100), 20, 250, 0);
   componentarr.push(
@@ -261,56 +373,56 @@ function draw() {
     }
     
     const parametersPanel = document.getElementById("parameters-panel");
-    parametersPanel.innerHTML = `
+//     parametersPanel.innerHTML = `
 
-     <tbody class="text-lg">
-            <tr>
-              <td>
-                <input
-                  class="input input-bordered input-primary px"
-                  style="width: 5.5em"
-                  oninput="UIEvents.bodyDataInputEditing(this)"
-                  type="number"
-                  step="1"
-                  min="0"
-                  data-bodyindex="-1"
-                />
-              </td>
-              <td>
-                <input
-                  class="input input-bordered input-primary py"
-                  style="width: 5.5em"
-                  oninput="UIEvents.bodyDataInputEditing(this)"
-                  type="number"
-                  step="1"
-                  min="0"
-                  data-bodyindex="-1"
-                />
-              </td>
-            </tr>
-          </tbody>
+//      <tbody class="text-lg">
+//             <tr>
+//               <td>
+//                 <input
+//                   class="input input-bordered input-primary px"
+//                   style="width: 5.5em"
+//                   oninput="UIEvents.bodyDataInputEditing(this)"
+//                   type="number"
+//                   step="1"
+//                   min="0"
+//                   data-bodyindex="-1"
+//                 />
+//               </td>
+//               <td>
+//                 <input
+//                   class="input input-bordered input-primary py"
+//                   style="width: 5.5em"
+//                   oninput="UIEvents.bodyDataInputEditing(this)"
+//                   type="number"
+//                   step="1"
+//                   min="0"
+//                   data-bodyindex="-1"
+//                 />
+//               </td>
+//             </tr>
+//           </tbody>
 
-<div class="relative mb-4 flex flex-wrap items-stretch bg-gray-600">
-  <div
-    class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 pb-[0.27rem] pt-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200">
-  </div>
-  <input
-    type="text"
-    class="relative m-0 block Width-[1px] min-Width-0 flex-auto rounded-r border border-solid border-neutral-300 bg-gray-600 bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-    aria-label="Text input with checkbox" />
-</div>
+// <div class="relative mb-4 flex flex-wrap items-stretch bg-gray-600">
+//   <div
+//     class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 pb-[0.27rem] pt-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200">
+//   </div>
+//   <input
+//     type="text"
+//     class="relative m-0 block Width-[1px] min-Width-0 flex-auto rounded-r border border-solid border-neutral-300 bg-gray-600 bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+//     aria-label="Text input with checkbox" />
+// </div>
 
-<div class="relative flex flex-wrap items-stretch">
-  <div
-    class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200">
+// <div class="relative flex flex-wrap items-stretch">
+//   <div
+//     class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200">
     
-  </div>
-  <input
-    type="text"
-    class="relative m-0 block Width-[1px] min-Width-0 flex-auto rounded-r border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-    aria-label="Text input with radio button" />
-</div>
-    `;
+//   </div>
+//   <input
+//     type="text"
+//     class="relative m-0 block Width-[1px] min-Width-0 flex-auto rounded-r border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+//     aria-label="Text input with radio button" />
+// </div>
+//     `;
     parametersPanel.classList.remove("hidden");
   }
   image(sandbox, 0,0);
@@ -349,8 +461,8 @@ function mousePressed(){
       selected = -1;
       setting(editables);
       const parametersPanel = document.getElementById("parameters-panel");
-    parametersPanel.innerHTML = "";
-    parametersPanel.classList.add("hidden");
+    //parametersPanel.innerHTML = "";
+    //parametersPanel.classList.add("hidden");
       }
       
     } else {
@@ -378,7 +490,7 @@ function findEditableData(obj){
     if (obj.type == "reflector"){
       return [obj, ["Position", obj.pos], ["Angle", obj.coords.theta], ["Width", obj.coords.magnitude * 2]]; //position, rotation, width
     } else if (obj.constructor.name == "Refractor"){
-      return [obj, ["Position", obj.pos], ["Angle", obj.coords.theta], ["Width", obj.coords.magnitude * 2], ["Refractive Coefficients", obj.rcoeffs]];
+      return [obj, ["Position", obj.pos], ["Angle", obj.coords.theta], ["Width", obj.coords.magnitude * 2], ["Thickness", obj.thickness], ["Refractive Coefficients", obj.rcoeffs]];
     } else if (obj.type == "CLens" || obj.type == "DLens"){
       return [obj, ["Position", obj.pos], ["Angle", obj.coords.theta], ["Width", obj.coords.magnitude * 2], ["Focal Length", obj.flength]];
     } else if (obj.constructor.name == "Prism"){
